9b185457402bed7d2bf1dcbf22e3e6ebfb781c5a
nd842 <andrei.smirnov@liflandgaming.com>
2018-10-22 23:44:59 +0300

    0.2.0 setup.py, updated readme

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="pytest-rt",
+	version="0.2.0",
+	author="Andrey Smirnov",
+	author_email="and.inbx@gmail.com",
+	description="pytest data collector plugin for Testgr",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/and-sm/pytest-rt",
+	py_modules=["pytest_rt"],
+	# the following makes a plugin available to pytest
+	entry_points={"pytest11": ["pytest_rt = pytest_rt"]},
+	classifiers=[
+		"Framework :: Pytest",
+		"Programming Language :: Python :: 3.6"],
+)
