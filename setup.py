0b582dfe53214ed129f838ea6fea5524e1ff8f06
Andrey Smirnov <and.inbx@gmail.com>
2022-05-05 16:39:11 +0300

    1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="pytest-rt",
-	version="1.3.0",
+	version="1.3.2",
 	author="Andrey Smirnov",
 	author_email="and.inbx@gmail.com",
 	description="pytest data collector plugin for Testgr",
@@ -19,3 +19,4 @@ setuptools.setup(
 		"Framework :: Pytest",
 		"Programming Language :: Python :: 3.8"],
 )
+
